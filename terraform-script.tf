eb522f3232a2747aa5871f2139afe685dc457b99
temi419 <134103258+temi419@users.noreply.github.com>
2023-05-26 16:35:28 -0400

    Add files via upload

---
+++ terraform-script.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "2.70.0"  # this is an older version of the AWS provider
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c94855ba95c574c8"  # Amazon Linux 2 LTS AMI
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "example-instance"
+  }
+}

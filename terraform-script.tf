4362e479624c1ad5a2cf7c8e551e7862c36c990b
temidevops1 <134103258+temidevops1@users.noreply.github.com>
2023-06-06 13:01:36 -0400

    Update terraform-script.tf

--- terraform-script.tf
+++ terraform-script.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.50.0"  # this is an older version of the AWS provider
+      version = "5.0.0"  # this is an older version of the AWS provider
     }
   }
 }

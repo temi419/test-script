94aabbc4f65a21b03008929204b6940dc60efd16
Temi-reno <339479+temi-reno@users.noreply.github.com>
2023-06-06 22:22:42 +0000

    Update Terraform aws to v2.70.4

--- terraform-script.tf
+++ terraform-script.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "2.0.0"  # this is an older version of the AWS provider lets see changes
+      version = "2.70.4"  # this is an older version of the AWS provider lets see changes
     }
   }
 }
